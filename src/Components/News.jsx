7b1e0f8420ce6f2a8960e11528f48b80f31135c1
ali.hocaoglu <ali.hocaoglu@kocholding.onmicrosoft.com>
2022-05-09 16:25:21 +0300

    update

---
+++ src/Components/News.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function News() {
+  return (
+    <div>Haberler Burada Listelenecek</div>
+  )
+}
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,7 +1,19 @@
+import { Container, Grid } from '@mui/material'
 import React from 'react'
+import Categories from '../Components/Categories'
+import News from '../Components/News'
 
 export default function HomePage() {
   return (
-    <div>HomePage</div>
+    <Container maxWidth="xl" style={{border:"1px solid red"}}>
+        <Grid container spacing={3} style={{marginTop:"30px"}}>
+            <Grid item xs={12} md={3} style={{border:"1px solid green"}}>
+                <Categories/>
+            </Grid>
+            <Grid item xs={12} md={9} style={{border:"1px solid green"}}>
+                <News/>
+            </Grid>
+        </Grid>
+    </Container>
   )
 }
